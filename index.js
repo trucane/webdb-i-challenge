d96d84fcb1a2a4857b37c25bad26782f9ed47930
trucane <trufashionista2013@gmail.com>
2019-07-15 16:39:58 -0500

    WIP initial commit, added server get, loaded dependedncies

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 4000;
 
+
+server.get('/', (req, res) =>{
+  res.send(`<h2>  We In the House </h2>`)
+});
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
